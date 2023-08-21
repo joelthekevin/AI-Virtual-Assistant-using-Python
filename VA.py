63af48eca35f03243f3e6b0c6c79b1dbea159384
Joel <92096050+joelthekevin@users.noreply.github.com>
2023-08-21 19:33:12 +0530

    VA.py

--- VA.py
+++ VA.py
@@ -1,7 +1,7 @@
-import pyttsx3
-import speech_recognition as sr 
+import pyttsx3                              # pip install pyttsx3
+import speech_recognition as sr             # pip install SpeechRecognition
 import datetime
-import wikipedia 
+import wikipedia                            # pip install wikipedia
 import webbrowser
 import os
 import smtplib
